8f5fc4156bc80271f79b0e5b25280e5990622603
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-03 07:46:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  subscription_id = "${AZURE_SUBSCRIPTION_ID}"
-  tenant_id       = "${AZURE_TENANT_ID}"
-  client_id       =  "${AZURE_CLIENT_ID}"
-  client_secret   =  "${AZURE_CLIENT_SECRET}"
- 
-  
-  }
 
 
 resource "azurerm_resource_group" "pwc-test" {
