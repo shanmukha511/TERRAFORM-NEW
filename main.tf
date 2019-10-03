1f663b2afb52822c26efaad4fa9a5ef4cad01788
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-03 14:22:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
   provider "azurerm" {
-  subscription_id = env.AZURE_SUBSCRIPTION_ID
-  tenant_id       = env.AZURE_TENANT_ID
-  client_id       =  env.AZURE_CLIENT_ID
-client_secret   =  env.AZURE_CLIENT_SECRET
+  subscription_id = "env.AZURE_SUBSCRIPTION_ID"
+  tenant_id       ="env.AZURE_TENANT_ID"
+  client_id       =  "env.AZURE_CLIENT_ID"
+client_secret   =  "env.AZURE_CLIENT_SECRET"
 
   
   }
