cbc0373b6cfacd1969c59674ae594d4553a2e6bf
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-04 01:26:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "pwc-test" {
 
 resource "azurerm_snapshot" "pwc-test" {
 
-  name                = "pwc-snapshot_name1"
+  name                = "pwc-snapshot_name2"
 
   location            = "eastus"
 
