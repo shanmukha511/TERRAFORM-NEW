93e453be61545022863a1e4f3c3b74bebc00f8fe
shanmukha511 <32700045+shanmukha511@users.noreply.github.com>
2019-10-08 02:57:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "azurerm_snapshot" "pwc-test" {
 
   create_option       = "Copy"
 
-  source_uri          = "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/pwc-test/providers/Microsoft.Compute/disks/VM1_OsDisk_1_651ba120ce06443a8af1eaf52025d04e"
+  source_uri          = "/subscriptions/a8a59231-c575-45fb-8e16-2b4c5b6a2271/resourceGroups/pwc-test/providers/Microsoft.Compute/disks/VM1_OsDisk_1_eb839d6126fb4e91aca879e0231ef8aa"
 
 }
 
